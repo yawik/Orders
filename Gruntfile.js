64b64d46e1d5247cc8e2f95435526a5e1a2ed605
Anthonius Munthi <me@itstoni.com>
2018-10-30 00:30:31 +0800

    added required grunt config

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,8 @@
-global.nodeModulesPath = __dirname + "/node_modules";
-
 module.exports = function(grunt) {
     require('load-grunt-tasks')(grunt);
     grunt.config.init({
-        targetDir: './test/sandbox/public'
+        targetDir: './test/sandbox/public',
+        nodeModulesPath: __dirname + "/node_modules"
     });
     grunt.loadTasks('./test/sandbox/public/modules/Core');
     grunt.registerTask('default', ['yawik:core']);
