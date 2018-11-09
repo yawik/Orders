b868da25a7db8bdefeb01fc4ebf9db32f464565b
Anthonius Munthi <me@itstoni.com>
2018-11-10 01:29:02 +0800

    reconfigured module structure, making sandbox more easy to configured

---
+++ test/sandbox/config/config.php
@@ -0,0 +1,17 @@
+<?php
+
+chdir(__DIR__.'/../');
+return [
+    'modules' => [
+        'Install',
+        'Core',
+        'Cv',
+        'Auth',
+        'Jobs',
+        'Applications',
+        'Organizations',
+        'Geo',
+        'Settings',
+        'Orders',
+    ],
+];
