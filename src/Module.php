156b8c53924db50acb04fa96c4755f3b48338f3b
Mathias Gelhausen <gelhausen@cross-solution.de>
2020-02-20 18:33:45 +0100

    Release 0.4.1

--- src/Module.php
+++ src/Module.php
@@ -25,7 +25,7 @@ class Module implements Feature\ConfigProviderInterface, VersionProviderInterfac
 {
     use VersionProviderTrait;
 
-    const VERSION = '0.4.0';
+    const VERSION = '0.4.1';
 
     public function getConfig()
     {
